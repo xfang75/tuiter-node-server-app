96a8bf546236577c7f98540d712feda2b34c4104
Xun Fang <xunfang@Xuns-MacBook-Air.local>
2022-11-30 20:14:21 -0800

    a9 Mongo fa22

--- app.js
+++ app.js
@@ -1,15 +1,26 @@
+
+
 import express from 'express'
 import cors from 'cors'
+import mongoose from "mongoose"
+//mongoose.connect('mongodb://localhost:27017/tuiter')
+
 import HelloController
   from "./controllers/hello-controller.js"
 import UserController
   from "./controllers/users/users-controller.js"
 import TuitsController
   from "./controllers/tuits/tuits-controller.js";
+
+
+const CONNECTION_STRING = process.env.DB_CONNECTION_STRING || 'mongodb://localhost:27017/tuiter'
+mongoose.connect(CONNECTION_STRING);
+
+
 const app = express();
 app.use(cors())
 app.use(express.json());
 TuitsController(app);
 HelloController(app);
 UserController(app);
-app.listen(process.env.PORT ||4000);
+app.listen(process.env.PORT || 4000);
